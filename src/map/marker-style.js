f6d4e9232c45b1afdd0c46f2583e62f34ac43908
Simon Evans <simon.evans@torchbox.com>
2018-03-24 12:25:25 +0000

    Refactor map structure and styles

---
+++ src/map/marker-style.js
@@ -0,0 +1,17 @@
+const markerStyle = {
+  position: 'absolute',
+  left: 12,
+  top: 12,
+  border: '5px solid #eaa115',
+  borderRadius: '50%',
+  backgroundColor: '#ab7510',
+  textAlign: 'center',
+  color: '#000',
+  fontSize: 0,
+  padding: 5,
+  width: 24,
+  height: 24,
+  boxShadow: '0px 0px 6px 2px rgba(156, 109, 18, 0.4)',
+};
+
+export { markerStyle };
